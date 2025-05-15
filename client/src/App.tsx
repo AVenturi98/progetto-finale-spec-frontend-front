b8e40afab1e116248ff8c6006947114da8c35f2f
AVenturi98 <lssndrventuri@gmail.com>
2025-05-15 07:51:57 +0200

    ADD BtnsFixed component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,6 +10,7 @@ import List from './components/List';
 import SettingModals from './components/SettingModals';
 import Modal from './components/Modal';
 import PopUp from './components/PopUp';
+import BtnsFixed from './components/BtnsFixed';
 
 // Icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -282,22 +283,9 @@ function App() {
         </section>
 
         {/* BUTTONS */}
-        <section className='fixed bottom-20 md:bottom-10 right-10 sm:right-20 xl:right-50 z-99'>
-          <div className='flex flex-col items-center gap-2'>
-            <button
-              type="button"
-              onClick={() => setFavoritesModal(true)}
-              className='rounded-full min-h-[40px] min-w-[40px] bg-[#4973fc] text-[#ffff00] hover:bg-[#0b43fa] hover:shadow-md shadow-[#4973fc]'>
-              ♥
-            </button>
-            <button
-              type="button"
-              className='rounded-full min-h-[40px] min-w-[40px] bg-[#4973fc] text-[#ffff00] hover:bg-[#0b43fa] hover:shadow-md shadow-[#4973fc]'>
-              +
-            </button>
-          </div>
-        </section>
+        <BtnsFixed setFavoritesModal={setFavoritesModal} />
 
+        {/* POP UP */}
         <PopUp
           isVisible={isVisible}
           popUp={popUp}
---
+++ client/src/components/BtnsFixed.tsx
@@ -0,0 +1,22 @@
+import type { SetStateAction } from "react";
+
+export default function BtnsFixed({ setFavoritesModal }: { setFavoritesModal: React.Dispatch<SetStateAction<boolean>> }) {
+
+    return (
+        <section className='fixed bottom-20 md:bottom-10 right-10 sm:right-20 xl:right-50 z-99'>
+            <div className='flex flex-col items-center gap-2'>
+                <button
+                    type="button"
+                    onClick={() => setFavoritesModal(true)}
+                    className='rounded-full min-h-[40px] min-w-[40px] bg-[#4973fc] text-[#ffff00] hover:bg-[#0b43fa] hover:shadow-md shadow-[#4973fc]'>
+                    ♥
+                </button>
+                <button
+                    type="button"
+                    className='rounded-full min-h-[40px] min-w-[40px] bg-[#4973fc] text-[#ffff00] hover:bg-[#0b43fa] hover:shadow-md shadow-[#4973fc]'>
+                    +
+                </button>
+            </div>
+        </section>
+    )
+}
